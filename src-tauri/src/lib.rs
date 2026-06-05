133ac36d17efb3cfa16edf02494d35f7a4488a79
Qiguiqiang <492754036@qq.com>
2026-06-05 15:32:54 +0800

    test: remove updater plugin entirely to isolate build failure

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -346,7 +346,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(app_state.clone())
         .invoke_handler(tauri::generate_handler![
             ping,
